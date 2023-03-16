ce8c93e529e5ded1057a4b7e24e84011b3596089
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-16 16:25:02 -0300

    :sparkles: feat: Create Theme

--- App.tsx
+++ App.tsx
@@ -3,19 +3,20 @@ import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/ro
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       
-      {fontsLoaded ? <View /> : <Loading />}
+      {!fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
