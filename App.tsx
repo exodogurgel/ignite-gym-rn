515ba362d8c969860fd65970ea66c16563d773dc
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-16 19:59:20 -0300

    :sparkles: feat: Create SignUp screen

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { NativeBaseProvider } from 'native-base';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
-import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -17,7 +17,7 @@ export default function App() {
         translucent
       />
       
-      {fontsLoaded ? <SignIn/> : <Loading />}
+      {fontsLoaded ? <SignUp/> : <Loading />}
     </NativeBaseProvider>
   );
 }
