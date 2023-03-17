08eb88ae859a0e523cce9d1808fbbfb20f75cb2c
Êxodo Gurgel <exodowellis@gmail.com>
2023-03-17 08:56:50 -0300

    :sparkles: feat: Create UserPhoto component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,25 @@
+import { VStack, HStack, Text, Heading } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto 
+        source={{ uri: 'http://github.com/exodogurgel.png' }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
+
+      <VStack>
+        <Text color="gray.100" fontSize="md">
+          olá,
+        </Text>
+
+        <Heading color="gray.100" fontSize="md">
+          Êxodo
+        </Heading>
+      </VStack>
+    </HStack>
+  )
+}
